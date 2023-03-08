bd4837b91d086789cfd10cbb24e97b37753fd22c
YuraMykoliv <126598493+YuraMykoliv@users.noreply.github.com>
2023-03-08 23:15:34 +0200

    -

--- src/Array_4.java
+++ src/Array_4.java
@@ -3,14 +3,14 @@ public class Array_4 {
     public static void main(String args[]) {
         int e[] = {3, 2, 3, 1, 4, 2, 8, 3};
 
-        int size[] = e.clone();
-        for (int i = 0; i < size.length; i++)
+        int e2[] = e.clone();
+        for (int i = 0; i < e2.length; i++)
 
-            for (int o = i + 1; o < size.length; o++) {
-                if (size[i] == size[o])
-                    size[o] = 0;
+            for (int o = i + 1; o < e2.length; o++) {
+                if (e2[i] == e2[o])
+                    e2[o] = 0;
             }
-        System.out.println(Arrays.toString(size));
+        System.out.println(Arrays.toString(e2));
     }
 }
 
